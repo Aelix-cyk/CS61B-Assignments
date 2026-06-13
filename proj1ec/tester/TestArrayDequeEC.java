cec023136ec7e9171bcaf86779d3c5a44097b4c6
aelix <17801023696@163.com>
2026-06-13 11:21:42 +0800

    done with proj1 autograder

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,106 @@
+package tester;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import student.StudentArrayDeque;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class TestArrayDequeEC {
+    @Test
+    public void randomizedTest() {
+       int N = 10000;
+       StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+       ArrayDequeSolution<Integer> refer = new ArrayDequeSolution<>();
+       StringBuilder message = new StringBuilder();
+
+       for (int i = 0; i < N; i += 1 ) {
+           int op = StdRandom.uniform(0, 5);
+           switch (op) {
+               case 0:
+                   sad1.addFirst(i);
+                   refer.addFirst(i);
+                   message.append("addFirst(" + i + ")\n");
+                   assertEquals(message.toString(), sad1.size(), refer.size());
+                   break;
+               case 1:
+                   sad1.addLast(i);
+                   refer.addLast(i);
+                   message.append("addLast(" + i + ")\n");
+                   assertEquals(message.toString(), sad1.size(), refer.size());
+                   break;
+               case 2:
+                   if (!sad1.isEmpty()) {
+                       message.append("removeFirst()\n");
+                       assertEquals(message.toString(), sad1.removeFirst(), refer.removeFirst());
+                   }
+                   break;
+               case 3:
+                   if (!sad1.isEmpty()) {
+                       message.append("removeLast()\n");
+                       assertEquals(message.toString(), sad1.removeLast(), refer.removeLast());
+                   }
+                   break;
+               case 4:
+                   if (!sad1.isEmpty()) {
+                       int idx = i % sad1.size();
+                       message.append("get(" + idx + ")\n");
+                       assertEquals(message.toString(), sad1.get(idx), refer.get(idx));
+                   }
+                   break;
+           }
+       }
+    }
+
+    @Test
+    public void erroredTest() {
+        StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> refer = new ArrayDequeSolution<>();
+
+        sad1.addFirst(0);
+        sad1.addFirst(1);
+        sad1.addLast(2);
+        sad1.addFirst(3);
+        sad1.addLast(4);
+        sad1.get(0);
+        sad1.addLast(6);
+        sad1.addFirst(7);
+        sad1.get(1);
+        sad1.removeFirst();
+        sad1.addLast(10);
+        sad1.addFirst(11);
+        sad1.removeFirst();
+        sad1.removeLast();
+        sad1.addFirst(14);
+        sad1.addFirst(15);
+        sad1.get(0);
+        sad1.removeFirst();
+        sad1.addFirst(18);
+        sad1.addFirst(19);
+        sad1.addFirst(20);
+        //sad1.removeLast();
+
+        refer.addFirst(0);
+        refer.addFirst(1);
+        refer.addLast(2);
+        refer.addFirst(3);
+        refer.addLast(4);
+        refer.get(0);
+        refer.addLast(6);
+        refer.addFirst(7);
+        refer.get(1);
+        refer.removeFirst();
+        refer.addLast(10);
+        refer.addFirst(11);
+        refer.removeFirst();
+        refer.removeLast();
+        refer.addFirst(14);
+        refer.addFirst(15);
+        refer.get(0);
+        refer.removeFirst();
+        refer.addFirst(18);
+        refer.addFirst(19);
+        refer.addFirst(20);
+
+        assertEquals(sad1.removeLast(), refer.removeLast());
+    }
+}
