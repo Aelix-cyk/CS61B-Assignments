19b62389cc8bb11104a7590cdf01b681ba7ff692
aelix <17801023696@163.com>
2026-08-05 13:27:55 +0800

    fix bugs

---
+++ proj3/byow/lab12/Position.java
@@ -0,0 +1,34 @@
+package byow.lab12;
+
+/** An immutable (x, y) coordinate pair. */
+public class Position {
+    public final int x;
+    public final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Position p = (Position) o;
+        return x == p.x && y == p.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+}
